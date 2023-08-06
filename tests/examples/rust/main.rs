a21b8332b6f2cc1bea789f000a742d94759b72f0
Zeioth <zeioth@hotmail.com>
2023-08-06 03:15:23 +0200

    chore(description) Adde to the rust example.

--- tests/examples/rust/main.rs
+++ tests/examples/rust/main.rs
@@ -1,3 +1,6 @@
+// Before testing rustdoc, you must run "cargo init".
+// Then you can generate the docs as "cargo rustdoc".
+// Then you can find them in "target/doc"
 fn main() {
     println!("Hello, World!");
 }
