ebce9b601eb0997fda47f5fd56f80ba0f4ae04a9
Zeioth <zeioth@hotmail.com>
2023-08-06 03:12:13 +0200

    chore(tests) Better godoc example.

--- tests/examples/go/main.go
+++ tests/examples/go/main.go
@@ -1,8 +1,33 @@
-package main
+/*
+To test, you should initialize this module first as "go mod init user".
+Then you can run godoc and visit it on localhost:6060/pkg/user
+*/
 
-import "fmt"
+// name of the package.
+package user
 
-func main() {
-    fmt.Println("Hello, World!")
+// fmt is responsible for formatting
+import (
+    "fmt"
+)
+
+// User is a struct of human data
+type User struct {
+    Age int
+    Name string
 }
 
+func main() {
+    // human is an initialization of the User struct
+    human := User {
+        Age: 0,
+        Name: "person",
+    }
+
+    fmt.Println(human.Talk())
+}
+
+// Talk is a method of the User struct
+func (receiver User) Talk() string {
+    return "Every User Gets to Say Something!"
+}
