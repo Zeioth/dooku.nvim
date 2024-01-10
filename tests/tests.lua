2ff0c2eb21b8b93b65787c46a3cfe4bd482b8848
Zeioth <zeioth@hotmail.com>
2024-01-10 21:13:13 +0100

    ✨feat(test suite): Added file to call all test files with a single command.

--- tests/tests.lua
+++ tests/tests.lua
@@ -1,2 +1,29 @@
--- Unit tests for dooku.nvim
--- TODO
+--- Test suite for dooku.nvim
+--- @usage :luafile ~/.local/share/nvim/lazy/dooku.nvim/tests/tests.lua
+--
+-- HOW TO OPEN THE TESTS LOG
+--   * Assuming you use nvim-notify run:
+--   :lua require("telescope").extensions.notify.notify()
+--
+-- EXPECTED RESULT
+--   * A webpage per test file will open in your internet browser.
+--   * If a test fail to pass, you will get a nvim notification.
+
+
+local utils = require("dooku.utils")
+local examples_dir = utils.os_path((debug.getinfo(1, 'S').source:sub(2):match '(.*/)') .. "/examples/")
+local tests_dir = utils.os_path((debug.getinfo(1, 'S').source:sub(2):match '(.*/)') .. "/tests/")
+
+-- Disable on_generate_open
+local config = require("dooku.config")
+config.set({ on_generate_open = false })
+
+-- Clean by resetting head for the examples directory
+-- os.execute("cd " .. examples_dir .. " ; git checkout -- .")
+
+-- Run test files
+dofile(tests_dir .. 'doxygen.lua')
+-- dofile(tests_dir .. 'rustdoc.lua')
+-- dofile(tests_dir .. 'godoc.lua')
+-- dofile(tests_dir .. 'typedoc.lua')
+-- dofile(tests_dir .. 'jsdoc.lua')
