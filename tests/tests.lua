2c4e6fab05bf1a37e421b93d61d44fed54f55e93
Zeioth <zeioth@hotmail.com>
2024-01-11 13:07:53 +0100

    ✅test(godoc): Basic test cases.

--- tests/tests.lua
+++ tests/tests.lua
@@ -14,16 +14,16 @@ local utils = require("dooku.utils")
 local examples_dir = utils.os_path((debug.getinfo(1, 'S').source:sub(2):match '(.*/)') .. "/examples/")
 local tests_dir = utils.os_path((debug.getinfo(1, 'S').source:sub(2):match '(.*/)') .. "/tests/")
 
--- Disable on_generate_open
+-- Clean
+os.execute("cd " .. examples_dir .. " ; git checkout -- .")
+
+-- Disable default opts
 local config = require("dooku.config")
 config.set({ on_generate_open = false })
 
--- Clean by resetting head for the examples directory
--- os.execute("cd " .. examples_dir .. " ; git checkout -- .")
-
--- Run test files
+-- Run tests
 dofile(tests_dir .. 'doxygen.lua')
 dofile(tests_dir .. 'rustdoc.lua')
--- dofile(tests_dir .. 'godoc.lua')
+dofile(tests_dir .. 'godoc.lua')
 -- dofile(tests_dir .. 'typedoc.lua')
 -- dofile(tests_dir .. 'jsdoc.lua')
---
+++ tests/tests/godoc.lua
@@ -0,0 +1,58 @@
+--- This test file run all supported cases of use.
+--- @usage :luafile ~/.local/share/nvim/lazy/dooku.nvim/tests/tests/godoc.lua
+
+local M = {}
+local ms = 1000 -- wait time
+local godoc = require("dooku.backends.godoc")
+local example_dir = require("dooku.utils").get_dooku_dir("tests/examples/go/")
+vim.fn.chdir(example_dir) -- set working_dir
+vim.notify("TESTING: Godoc backend", vim.log.levels.INFO)
+
+
+-- TEST: godoc.auto_setup()
+--=============================================================================
+function M.test_auto_setup()
+  -- ARRANGE
+  vim.notify("Testing `godoc.auto_setup()`.", vim.log.levels.INFO)
+
+  -- ACT
+  godoc.auto_setup()
+  vim.wait(ms)
+end
+
+
+-- TEST: godoc.generate()
+--=============================================================================
+function M.test_generate()
+  -- ARRANGE
+  vim.notify("Testing `godoc.generate(false)`.", vim.log.levels.INFO)
+
+  -- ACT
+  godoc.generate(false)
+  vim.wait(ms)
+end
+
+
+-- TEST: godoc.open()
+--=============================================================================
+function M.test_open()
+  -- ARRANGE
+  vim.notify("Testing `godoc.open()`.", vim.log.levels.INFO)
+
+  -- ACT
+  godoc.open()
+  vim.wait(ms)
+
+  -- ASSERT (This step is currently visually asserted)
+  vim.notify(
+    "Please, check on your internet browser that godoc opened correctly.",
+    vim.log.levels.WARN)
+end
+
+
+-- RUN TESTS
+--=======================================================================
+M.test_auto_setup()
+M.test_generate()
+M.test_open()
+
