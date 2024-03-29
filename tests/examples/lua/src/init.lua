3181e8fcd9440a39663c58e549f5c2e268887133
Zeioth <zeioth@hotmail.com>
2024-03-29 14:53:09 +0100

    tests(ldoc): Added

---
+++ tests/examples/lua/src/init.lua
@@ -0,0 +1,10 @@
+-- hello.lua
+-- Simple Lua script to print "Hello, World!" to the console.
+
+--- Prints "Hello, World!" to the console.
+-- This function prints the famous "Hello, World!" message to the standard output.
+local function hello()
+    print("Hello, World!")
+end
+
+return hello
---
+++ tests/tests/ldoc.lua
@@ -0,0 +1,59 @@
+--- This test file run all supported cases of use.
+--- @usage :luafile ~/.local/share/nvim/lazy/dooku.nvim/tests/tests/ldoc.lua
+
+local M = {}
+local ms = 2000 -- wait time
+local ldoc = require("dooku.backends.ldoc")
+local example_dir = require("dooku.utils").get_dooku_dir("tests/examples/lua/")
+
+vim.fn.chdir(example_dir) -- set working_dir
+vim.notify("TESTING: Ldoc backend", vim.log.levels.INFO)
+
+
+-- TEST: ldoc.auto_setup()
+--=============================================================================
+function M.test_auto_setup()
+  -- ARRANGE
+  vim.notify("Testing `ldoc.auto_setup()`.", vim.log.levels.INFO)
+
+  -- ACT
+  ldoc.auto_setup()
+  vim.wait(ms)
+end
+
+
+-- TEST: ldoc.generate()
+--=============================================================================
+function M.test_generate()
+  -- ARRANGE
+  vim.notify("Testing `ldoc.generate(false)`.", vim.log.levels.INFO)
+
+  -- ACT
+  ldoc.generate(false)
+  vim.wait(ms)
+end
+
+
+-- TEST: ldoc.open()
+--=============================================================================
+function M.test_open()
+  -- ARRANGE
+  vim.notify("Testing `ldoc.open()`.", vim.log.levels.INFO)
+
+  -- ACT
+  ldoc.open()
+  vim.wait(ms)
+
+  -- ASSERT (This step is currently visually asserted)
+  vim.notify(
+    "Please, check on your internet browser that ldoc opened correctly.",
+    vim.log.levels.WARN)
+end
+
+
+-- RUN TESTS
+--=======================================================================
+M.test_auto_setup()
+M.test_generate()
+M.test_open()
+
